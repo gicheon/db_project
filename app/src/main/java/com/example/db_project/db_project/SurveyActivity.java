32c092bfe852283b2afce6af057be076fb71ca2a
gicheonkang <kgc519@gmail.com>
2017-12-07 18:10:09 +0900

    rating bar added

--- app/src/main/java/com/example/db_project/db_project/SurveyActivity.java
+++ app/src/main/java/com/example/db_project/db_project/SurveyActivity.java
@@ -1,10 +1,14 @@
 package com.example.db_project.db_project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 
 /**
@@ -13,12 +17,24 @@ import android.widget.Button;
 
 public class SurveyActivity extends Activity{
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
-    }
 
+        //final TextView tv = (TextView)findViewById(R.id.)
+        RatingBar rb1 = (RatingBar)findViewById(R.id.ratingbar1);
+
+
+        rb1.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating,
+                                        boolean fromUser) {
+                //tv.setText("rating : " + rating);
+            }
+        });
+    }
 
 
 
